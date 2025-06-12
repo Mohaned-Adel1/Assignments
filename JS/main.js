1168410592557b8070dc7de32b386cda0768515c
Mohaned-Adel1 <Mohanedadelelsayed@gmail.com>
2025-06-12 22:29:29 +0300

    Bookmark assignment

--- JS/main.js
+++ JS/main.js
@@ -1,52 +1,61 @@
-let quotes = [
-  {
-    qoute: "Be yourself; everyone else is already taken.",
-    author: "Oscar Wilde",
-  },
-  {
-    qoute:
-      "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best",
-    author: "Marilyn Monroe",
-  },
-  { qoute: "So many books, so little time.", author: " Frank Zappa" },
-  {
-    qoute:
-      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe",
-    author: " Albert Einstein",
-  },
-  {
-    qoute: "A room without books is like a body without a soul.",
-    author: "Marcus Tullius Cicero",
-  },
-  {
-    qoute:
-      "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
-    author: "Bernard M. Baruch",
-  },
-  {
-    qoute: "You only live once, but if you do it right, once is enough.",
-    author: "Mae West",
-  },
-  {
-    qoute: "Be the change that you wish to see in the world.",
-    author: " Mahatma Gandhi",
-  },
-  {
-    qoute:
-      "In three words I can sum up everything I've learned about life: it goes on.",
-    author: "Robert Frost",
-  },
-];
-let lastIndex;
+let webSitesData = document.getElementById("webSitesData");
 
-function genQuote() {
-  let newIndex;
-  do {
-    newIndex = Math.floor(Math.random() * quotes.length);
-  } while (newIndex === lastIndex);
-  let qouteBody = quotes[newIndex].qoute;
-  let qouteAuth = quotes[newIndex].author;
-  lastIndex = newIndex;
-  document.getElementById("quote").innerHTML = `"${qouteBody}"`;
-  document.getElementById("author").innerHTML = `-- ${qouteAuth}`;
+if (!localStorage.getItem("bookmarksList")) {
+  localStorage.setItem("bookmarksList", JSON.stringify([]));
+}
+
+let bookmarks = JSON.parse(localStorage.getItem("bookmarksList"));
+
+bookmarkSync(bookmarks);
+function addWebsite() {
+  let siteName = document.getElementById("siteName").value;
+  let siteUrl = document.getElementById("siteURL").value;
+
+  bookmarks.push({
+    siteName: `${siteName}`,
+    SiteURL: `${siteUrl}`,
+  });
+  localStorage.setItem("bookmarksList", JSON.stringify(bookmarks));
+
+  bookmarkSync(bookmarks);
+  document.getElementById("siteName").value = "";
+  document.getElementById("siteURL").value = "";
+}
+
+function deleteSite(bookIndex) {
+  bookmarks.splice(bookIndex, 1);
+  localStorage.setItem("bookmarksList", JSON.stringify(bookmarks));
+  bookmarkSync(bookmarks);
+}
+
+function bookmarkSync(listOfBookmarks) {
+  let siteHTML = "";
+  for (let i = 0; i < listOfBookmarks.length; i++) {
+    let newSite = `
+    <tr>
+  <td>${i + 1}</td>
+          <td>${listOfBookmarks[i].siteName}</td>
+          <td>
+            <button class="btn btn-success text-white " onclick="visitURL('${
+              listOfBookmarks[i].SiteURL
+            }')">
+              <i class="fa-solid fa-eye pe-2"></i>
+              Visit
+            </button>
+          </td>
+          <td>
+            <button class="btn btn-danger text-white" onclick="deleteSite(${i})">
+              <i class="fa-solid fa-trash-can pe-2"></i>
+              Delete
+            </button>
+          </td></tr>
+  
+  `;
+    siteHTML += newSite;
+  }
+  webSitesData.innerHTML = siteHTML;
+}
+function visitURL(url) {
+  console.log(url);
+  window.open(url, "_blank");
 }
